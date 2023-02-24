e991477c860db01598d047aa9daaa404e3eb2988
Cwatts <cadenwatts111@gmail.com>
2023-02-24 13:54:25 -0700

    final changes 2/24/23

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,9 +1,12 @@
 package com.example.project;
 
+import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Cube {
 
@@ -34,93 +37,6 @@ public class Cube {
 	}
 	};
 
-	// static String[][] face = {
-	// {"1","2","3"},
-	// {"4","5","6"},
-	// {"7","8","9"}
-	// };
-
-	// static void move(String[][][] Cube, String input){
-	// switch(input){
-	// case "u":
-	// break;
-
-	// case "l":
-	// break;
-
-	// case "d":
-	// break;
-
-	// case "r":
-	// break;
-
-	// case "f":
-	// break;
-
-	// case "b":
-	// break;
-	// }
-	// }
-
-	// static void show(String[][] Face){
-	// for(String[] i: Face){
-	// for(String j: i){
-	// System.out.print(j);
-	// }
-	// System.out.println();
-	// }
-	// }
-
-	// static void rotateFace(String[][]Face, boolean clockwise){
-
-	// String[][] tempFace = new String[3][3];
-
-	// for(int i=0; i<3; i++)
-	// for(int j=0; j<3; j++)
-	// tempFace[i][j] = Face[i][j];
-
-	// if(clockwise){
-	// //order is Face[y][x] or Face[row][col]
-
-	// // Below comments were another solution provided by S.Luther
-	// // if(clockwise){
-	// // rotateFace(face, false);
-	// // rotateFace(face, false);
-	// // rotateFace(face, false);
-	// // }else{
-	// // for(int i=0; i<3;i++)
-	// // for(int j=0; j<3;j++)
-	// // Face[i][j] = tempFace[j][i];
-	// // String[] temp = new String[3];
-	// // for(int k=0; k<3; k++)
-	// // temp[k] = Face[0][k];
-	// // Face[0] = Face[2];
-
-	// // Face[2] = temp;
-
-	// Face[0][0] = tempFace[2][0];
-
-	// Face[0][1] = tempFace[1][0];
-	// Face[0][2] = tempFace[0][0];
-	// Face[1][0] = tempFace[2][1];
-	// Face[1][1] = tempFace[1][1];
-	// Face[1][2] = tempFace[0][1];
-	// Face[2][0] = tempFace[2][2];
-	// Face[2][1] = tempFace[1][2];
-	// Face[2][2] = tempFace[0][2];
-	// } else {
-	// Face[0][0] = tempFace[0][2];
-
-	// Face[0][1] = tempFace[1][2];
-	// Face[0][2] = tempFace[2][2];
-	// Face[1][0] = tempFace[0][1];
-	// Face[1][1] = tempFace[1][1];
-	// Face[1][2] = tempFace[2][1];
-	// Face[2][0] = tempFace[0][0];
-	// Face[2][1] = tempFace[1][0];
-	// Face[2][2] = tempFace[2][0];
-	// }
-	// }
 
 	class edgedFace {
 		public int current_face;
@@ -181,9 +97,9 @@ public class Cube {
 				case 2: // orange face
 
 					edge1[0] = 3; // green
-					edge1[1] = 0;
-					edge1[2] = 3;
-					edge1[3] = 6;
+					edge1[1] = 2;
+					edge1[2] = 5;
+					edge1[3] = 8;
 
 					edge2[0] = 4; // yellow
 					edge2[1] = 0;
@@ -209,9 +125,9 @@ public class Cube {
 					edge1[3] = 8;
 
 					edge2[0] = 4; // yellow
-					edge2[1] = 2;
-					edge2[2] = 5;
-					edge2[3] = 8;
+					edge2[1] = 0;
+					edge2[2] = 1;
+					edge2[3] = 2;
 
 					edge3[0] = 2; // orange
 					edge3[1] = 0;
@@ -219,8 +135,8 @@ public class Cube {
 					edge3[3] = 6;
 
 					edge4[0] = 5; // white.
-					edge4[1] = 2;
-					edge4[2] = 5;
+					edge4[1] = 6;
+					edge4[2] = 7;
 					edge4[3] = 8;
 					break;
 
@@ -232,9 +148,9 @@ public class Cube {
 					edge1[3] = 2;
 
 					edge2[0] = 3; // green
-					edge2[1] = 0;
-					edge2[2] = 1;
-					edge2[3] = 2;
+					edge2[1] = 6;
+					edge2[2] = 7;
+					edge2[3] = 8;
 
 					edge3[0] = 0; // red
 					edge3[1] = 0;
@@ -248,23 +164,23 @@ public class Cube {
 					break;
 				case 5: // white face
 					edge1[0] = 2; // orange
-					edge1[1] = 2;
-					edge1[2] = 5;
+					edge1[1] = 6;
+					edge1[2] = 7;
 					edge1[3] = 8;
 
 					edge2[0] = 1; // blue
-					edge2[1] = 2;
-					edge2[2] = 5;
+					edge2[1] = 6;
+					edge2[2] = 7;
 					edge2[3] = 8;
 
 					edge3[0] = 0; // red
-					edge3[1] = 0;
-					edge3[2] = 3;
-					edge3[3] = 6;
+					edge3[1] = 6;
+					edge3[2] = 7;
+					edge3[3] = 8;
 
 					edge4[0] = 3; // green.
-					edge4[1] = 2;
-					edge4[2] = 5;
+					edge4[1] = 6;
+					edge4[2] = 7;
 					edge4[3] = 8;
 					break;
 			}
@@ -290,7 +206,7 @@ public class Cube {
 				cube[eFace.current_face][3] = copy[eFace.current_face][1];
 				cube[eFace.current_face][5] = copy[eFace.current_face][7];
 				cube[eFace.current_face][6] = copy[eFace.current_face][0];
-				cube[eFace.current_face][7] = copy[eFace.current_face][5];
+				cube[eFace.current_face][7] = copy[eFace.current_face][3];
 				cube[eFace.current_face][8] = copy[eFace.current_face][6];
 
 				// edge 1 changed
@@ -365,9 +281,10 @@ public class Cube {
 	public static void main(final String[] args)
 			throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList commands = new ArrayList();
 
 		Cube myCube = new Cube();
+		ArrayList<String> commands = new ArrayList<>();
+		ArrayList<String> requiredMoves = new ArrayList<>();
 
 		boolean argsCheck = false;
 		int argsRunIndex = 0;
@@ -397,72 +314,97 @@ public class Cube {
 				case "u":// yellow face
 					myCube.turnFace(4, "c");
 					myCube.showCube();
+					commands.add("u");
+					requiredMoves.add("u'");
 					break;
 
 				case "l":// orange face
 					myCube.turnFace(2, "c");
 					myCube.showCube();
+					commands.add("l");
+					requiredMoves.add("l'");
 					break;
 
 				case "d":// white face
 					myCube.turnFace(5, "c");
 					myCube.showCube();
+					commands.add("d");
+					requiredMoves.add("d'");
 					break;
 
 				case "r":// red face
 					myCube.turnFace(0, "c");
 					myCube.showCube();
+					commands.add("r");
+					requiredMoves.add("r'");
 					break;
 
 				case "f":// blue face
 					myCube.turnFace(1, "c");
 					myCube.showCube();
+					commands.add("f");
+					requiredMoves.add("f'");
 					break;
 
 				case "b":// green face
 					myCube.turnFace(3, "c");
 					myCube.showCube();
+					commands.add("b");
+					requiredMoves.add("b'");
 					break;
 
 				case "u'":
 					myCube.turnFace(4, "cc");
 					myCube.showCube();
+					commands.add("u'");
+					requiredMoves.add("u");
 					break;
 
 				case "l'":
 					myCube.turnFace(2, "cc");
 					myCube.showCube();
+					commands.add("l'");
+					requiredMoves.add("l");
 					break;
 
 				case "d'":
 					myCube.turnFace(5, "cc");
 					myCube.showCube();
+					commands.add("d'");
+					requiredMoves.add("d");
 					break;
 
 				case "r'":
 					myCube.turnFace(0, "cc");
 					myCube.showCube();
+					commands.add("r'");
+					requiredMoves.add("r");
 					break;
 
 				case "f'":
 					myCube.turnFace(1, "cc");
 					myCube.showCube();
+					commands.add("f'");
+					requiredMoves.add("f");
 					break;
 
 				case "b'":
 					myCube.turnFace(3, "cc");
 					myCube.showCube();
+					commands.add("b'");
+					requiredMoves.add("b");
 					break;
 
 				case "s": // This is meant to solve the cube.
-				
+					System.out.println(commands);
+					Collections.reverse(requiredMoves);
+					System.out.println(requiredMoves);
 					break;
 
 				case "q":
 					proceed = false;
 					break;
 			}
-
 		}
 	}
 }
